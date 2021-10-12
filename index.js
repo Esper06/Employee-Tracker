d3acf25575fda07c110c92c3bd6efa0525e4a62b
Esper06 <matt.walford98@gmail.com>
2021-10-12 19:41:37 +0100

    updated module packages. Changed password in const connection to be env based

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: process.env.MYSQL_PASSWORD,
     database: "employee_trackerDB"
   });
 
