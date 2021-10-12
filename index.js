7e9959f94a770e2709301eb7f3f8cfdf60c49cfb
Esper06 <matt.walford98@gmail.com>
2021-10-12 19:23:02 +0100

    Added select prompts

--- index.js
+++ index.js
@@ -99,4 +99,30 @@ function viewAllDepartments() {
       startPrompt()
     })
   }
-  
+
+  //================= Select Role Quieries Role Title for Add Employee Prompt ===========//
+var roleArr = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
+
+  })
+  return roleArr;
+}
+
+
+//================= Select Role Quieries The Managers for Add Employee Prompt ===========//
+var managersArr = [];
+function selectManager() {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managersArr.push(res[i].first_name);
+    }
+
+  })
+  return managersArr;
+}
