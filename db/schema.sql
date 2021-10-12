254adfedf0e808368ddc78bc3b437f282fbf9159
Esper06 <matt.walford98@gmail.com>
2021-10-12 20:14:35 +0100

    changed the null thing back in the schema as it was causing bugs

--- db/schema.sql
+++ db/schema.sql
@@ -23,8 +23,8 @@ CREATE TABLE role (
 -- EMPLOYEE ROLE TABLE ----
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name NOT NULL VARCHAR(30),
-  last_name NOT NULL VARCHAR(30),
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   manager_id INT,
   role_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
